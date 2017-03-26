425d46a5742e0419e01a94d95e80a6efa828a114
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-26 01:37:02 -0300

    Use env for mongodb

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node
 
-RUN npm install -g configurable-http-proxy
+COPY package.json .
+RUN npm install
+COPY . .
 
-ENV CONFIGPROXY_AUTH_TOKEN=123
-
-CMD configurable-http-proxy --port $PORT --api-port 8001 --default-target=http://localhost:8001
+CMD MONGODB_URI=$MONGODB_URI PORT=$PORT node server.js
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
 
 const server = express();
-const mongoUrl = 'mongodb://localhost:27017/microfrontends-router';
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/microfrontends-router';
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
